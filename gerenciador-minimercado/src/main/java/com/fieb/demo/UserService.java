4f8f26039bb49c00a7cb528b7bf5e6557799c836
rm87868 <rm87868@BF-LAB04-MV-M01.fieb.local>
2023-03-06 21:55:01 -0300

    okay

---
+++ gerenciador-minimercado/src/main/java/com/fieb/demo/UserService.java
@@ -0,0 +1,10 @@
+package com.fieb.demo;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.security.core.userdetails;
